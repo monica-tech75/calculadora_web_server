af0b8993e24f23660751768b74b40c3f4aa03a23
Monica Pardo <moonyruna3@gmail.com>
2024-12-21 10:41:40 +0100

    Realice un serveidor web donde se sirve una web de una calculadora

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const { envs } = require('./config/env')
+const { startServer } = require('./server/server')
+
+
+const main = () => {
+  startServer({
+    port: envs.PORT,
+    public_path: envs.PUBLIC_PATH
+  })
+}
+
+
+// funcion agnostica autoconvocada
+// Agnostica porque no tiene nombre
+// Autoconvocada porque la ejecutamos con los parentesis
+(async () => {
+  main()
+})()
